77a7d395f2e2c5f9932e888092e0ef3320f87037
Leandro Tk <leandrotk100@gmail.com>
2017-10-31 23:30:31 -0200

    Extract board class from tic-tac-toe game

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,31 +1,44 @@
-class Game
-  attr_reader :board, :computer, :human
+class Board
+  attr_reader :spots
 
   def initialize
-    @board = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
-    @computer = 'X'
-    @human = 'O'
+    @spots = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+  end
+
+  def choose_spot(index, mark)
+    spots[index] = mark
+  end
+end
+
+class Game
+  attr_reader :board
+
+  PLAYER_1 = 'X'
+  PLAYER_2 = 'O'
+
+  def initialize
+    @board = Board.new
   end
 
   def start_game
-    puts " #{board[0]} | #{board[1]} | #{board[2]} \n===+===+===\n #{board[3]} | #{board[4]} | #{board[5]} \n===+===+===\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+    puts " #{board_spots[0]} | #{board_spots[1]} | #{board_spots[2]} \n===+===+===\n #{board_spots[3]} | #{board_spots[4]} | #{board_spots[5]} \n===+===+===\n #{board_spots[6]} | #{board_spots[7]} | #{board_spots[8]} \n"
     puts "\nEnter [0-8]:\n"
 
-    until game_over?(board) || tie?(board)
-      get_human_spot
-      eval_board if !game_over?(board) && !tie?(board)
-      puts " #{board[0]} | #{board[1]} | #{board[2]} \n===+===+===\n #{board[3]} | #{board[4]} | #{board[5]} \n===+===+===\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+    until game_over? || tie?
+      choose_player_spot
+      eval_board if !game_over? && !tie?
+      puts " #{board_spots[0]} | #{board_spots[1]} | #{board_spots[2]} \n===+===+===\n #{board_spots[3]} | #{board_spots[4]} | #{board_spots[5]} \n===+===+===\n #{board_spots[6]} | #{board_spots[7]} | #{board_spots[8]} \n"
     end
 
     puts "\nGame over"
   end
 
-  def get_human_spot
+  def choose_player_spot
     spot = nil
     until spot
       spot = gets.chomp.to_i
-      if board[spot] != 'X' && board[spot] != 'O'
-        board[spot] = human
+      if board_spots[spot] != 'X' && board_spots[spot] != 'O'
+        board.choose_spot(spot, PLAYER_2)
       else
         spot = nil
       end
@@ -35,13 +48,13 @@ class Game
   def eval_board
     spot = nil
     until spot
-      if board[4] == '4'
+      if board_spots[4] == '4'
         spot = 4
-        board[spot] = computer
+        board.choose_spot(spot, PLAYER_1)
       else
-        spot = get_best_move(board, computer)
-        if board[spot] != 'X' && board[spot] != 'O'
-          board[spot] = computer
+        spot = get_best_move(PLAYER_1)
+        if board_spots[spot] != 'X' && board_spots[spot] != 'O'
+          board.choose_spot(spot, PLAYER_1)
         else
           spot = nil
         end
@@ -49,28 +62,28 @@ class Game
     end
   end
 
-  def get_best_move(board, next_player, depth = 0, best_score = {})
+  def get_best_move(next_player, depth = 0, best_score = {})
     available_spaces = []
     best_move = nil
 
-    board.each do |s|
+    board_spots.each do |s|
       available_spaces << s if s != 'X' && s != 'O'
     end
 
     available_spaces.each do |as|
-      board[as.to_i] = computer
-      if game_over?(board)
+      board_spots[as.to_i] = PLAYER_1
+      if game_over?
         best_move = as.to_i
-        board[as.to_i] = as
+        board_spots[as.to_i] = as
         return best_move
       else
-        board[as.to_i] = human
-        if game_over?(board)
+        board_spots[as.to_i] = PLAYER_2
+        if game_over?
           best_move = as.to_i
-          board[as.to_i] = as
+          board_spots[as.to_i] = as
           return best_move
         else
-          board[as.to_i] = as
+          board_spots[as.to_i] = as
         end
       end
     end
@@ -81,19 +94,25 @@ class Game
     available_spaces[n].to_i
   end
 
-  def game_over?(board)
-    [board[0], board[1], board[2]].uniq.length == 1 ||
-    [board[3], board[4], board[5]].uniq.length == 1 ||
-    [board[6], board[7], board[8]].uniq.length == 1 ||
-    [board[0], board[3], board[6]].uniq.length == 1 ||
-    [board[1], board[4], board[7]].uniq.length == 1 ||
-    [board[2], board[5], board[8]].uniq.length == 1 ||
-    [board[0], board[4], board[8]].uniq.length == 1 ||
-    [board[2], board[4], board[6]].uniq.length == 1
+  private
+
+  def game_over?
+    [board_spots[0], board_spots[1], board_spots[2]].uniq.size == 1 ||
+    [board_spots[3], board_spots[4], board_spots[5]].uniq.size == 1 ||
+    [board_spots[6], board_spots[7], board_spots[8]].uniq.size == 1 ||
+    [board_spots[0], board_spots[3], board_spots[6]].uniq.size == 1 ||
+    [board_spots[1], board_spots[4], board_spots[7]].uniq.size == 1 ||
+    [board_spots[2], board_spots[5], board_spots[8]].uniq.size == 1 ||
+    [board_spots[0], board_spots[4], board_spots[8]].uniq.size == 1 ||
+    [board_spots[2], board_spots[4], board_spots[6]].uniq.size == 1
   end
 
-  def tie?(board)
-    board.all? { |cell| cell == 'X' || cell == 'O' }
+  def tie?
+    board_spots.all? { |spot| spot == 'X' || spot == 'O' }
+  end
+
+  def board_spots
+    board.spots
   end
 end
 
