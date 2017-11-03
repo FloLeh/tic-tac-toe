3b9e0946ff5031b54fcb28cbf601da8e4565738c
Leandro Tk <leandrotk100@gmail.com>
2017-11-03 18:21:26 -0200

    Implemented game level input

--- lib/input_validator.rb
+++ lib/input_validator.rb
@@ -17,11 +17,43 @@ module InputValidator
     game_type.to_sym
   end
 
+  def choose_game_level
+    puts "Choose the game level:"
+    puts "'0' for easy"
+    puts "'1' for medium"
+    puts "'2' for hard"
+    game_level = gets.chomp
+
+    while wrong_game_level(game_level)
+      puts "\nYou entered '#{game_level}'. Choose these game level::\n"
+      puts "'0' for easy"
+      puts "'1' for medium"
+      puts "'2' for hard"
+      game_level = gets.chomp
+    end
+
+    parse_game_level(game_level)
+  end
+
   private
 
   def wrong_game_type(game_type)
     game_type != 'cc' &&
     game_type != 'hh' &&
     game_type != 'hc'
+  end
+
+  def wrong_game_level(game_level)
+    game_level != '0' &&
+    game_level != '1' &&
+    game_level != '2'
+  end
+
+  def parse_game_level(level)
+    {
+      '0' => :easy,
+      '1' => :medium,
+      '2' => :hard
+    }[level]
   end
 end
